d011089cb8d72c0727b3cefb8517ce774156112a
mohhasbi <81578584+mohhasbi@users.noreply.github.com>
2021-06-01 00:25:56 +0700

    Add files via upload

---
+++ latihan1.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 31 Bulan Mei 2021 pada 19.25
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id_barang` int(10) NOT NULL,
+  `kategori` varchar(30) DEFAULT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  `harga_beli` decimal(10,0) DEFAULT NULL,
+  `harga_jual` decimal(10,0) DEFAULT NULL,
+  `stok` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_barang`
+--
+
+INSERT INTO `data_barang` (`id_barang`, `kategori`, `nama`, `gambar`, `harga_beli`, `harga_jual`, `stok`) VALUES
+(1, 'Elektronik', 'HP Samsung Android', 'hp_samsung.jpg', '2000000', '2400000', 5),
+(2, 'Elektronik', 'HP Xiaomi Android', 'hp_xiaomi.jpg', '1000000', '1400000', 5),
+(3, 'Elektronik', 'HP OPPO Android', 'hp_oppo.jpg', '1800000', '2300000', 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `data_barang`
+--
+ALTER TABLE `data_barang`
+  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
